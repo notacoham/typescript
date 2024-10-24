68d64bda14bb321a14d7235a8c54c576ee786a3c
Alex Cottam <alexcottam12@gmail.com>
2024-10-24 07:17:52 -0600

    add functionality

--- src/tasks.ts
+++ src/tasks.ts
@@ -7,4 +7,58 @@ type Task = {
   isCompleted: boolean;
 };
 
-const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(renderTask);
+
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
+
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = formInput?.value;
+  if (taskDescription) {
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    // add task to list
+    addTask(task);
+    // render tasks
+    renderTask(task);
+    // update local storage
+    updateStorage();
+    formInput.value = '';
+    return;
+  }
+  alert('Please enter a task description.');
+});
+
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
+}
+
+function renderTask(task: Task) {
+  const taskElement = document.createElement('li');
+  taskElement.textContent = task.description;
+
+  const taskCheckbox = document.createElement('input');
+  taskCheckbox.type = 'checkbox';
+  taskCheckbox.checked = task.isCompleted;
+
+  taskCheckbox.addEventListener('change', () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+
+  taskElement.appendChild(taskCheckbox);
+
+  taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage(): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
